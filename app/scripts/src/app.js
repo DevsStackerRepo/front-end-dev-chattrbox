44c0a453649fa3997aeb7411368b927d7ebcef0f
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-25 08:24:47 +0000

    Creating the ChatList Class

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,11 +1,13 @@
 import socket from './ws-client';
-import {ChatForm} from './dom';
+import {ChatForm, ChatList} from './dom';
 const FORM_SELECTOR = '[data-chat="chat-form"]';
 const INPUT_SELECTOR = '[data-chat="message-input"]';
+const LIST_SELECTOR = '[data-chat="message-list"]';
 
 class ChatApp {
     constructor() {
     this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+    this.chatList = new ChatList(LIST_SELECTOR, 'wonderwoman');
 
     socket.init('ws://localhost:3001');
     socket.registerOpenHandler(() => {
@@ -16,6 +18,8 @@ class ChatApp {
     });
       socket.registerMessageHandler((data) => {
         console.log(data);
+        let message = new ChatMessage(data);
+        this.chatList.drawMessage(message.serialize());
       });
   } }
 class ChatMessage {
