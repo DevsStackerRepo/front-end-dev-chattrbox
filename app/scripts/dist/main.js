4a9e115bae1e743f03998b717ee13f5445015a8b
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-25 07:59:15 +0000

    Error: Plugin/Preset files are not allowed to export objects, only functions.

--- app/scripts/dist/main.js
+++ app/scripts/dist/main.js
@@ -1,11 +1,1 @@
-'use strict';
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var ChatApp = function ChatApp() {
-    _classCallCheck(this, ChatApp);
-
-    console.log('Hello ES6!');
-};
-
-new ChatApp();
+console.error("Error: Plugin/Preset files are not allowed to export objects, only functions. In /Users/Mutaki/Desktop/chattrbox/node_modules/babel-preset-es2015/lib/index.js while parsing file: /Users/Mutaki/Desktop/chattrbox/app/scripts/src/main.js");
