92f973ab3bd9a7d392635ef677cc2ee628963d3a
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-25 08:09:18 +0000

    Sending and echoing a message

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,9 +1,15 @@
 import socket from './ws-client';
 class ChatApp {
     constructor() {
-        socket.init('ws://localhost:3001');
-      }
-}
+      socket.init('ws://localhost:3001');
+      socket.registerOpenHandler(() => {
+        let message = new ChatMessage({ message: 'pow!' });
+        socket.sendMessage(message.serialize());
+      });
+      socket.registerMessageHandler((data) => {
+        console.log(data);
+      });
+  } }
 class ChatMessage {
     constructor({
     message: m,
