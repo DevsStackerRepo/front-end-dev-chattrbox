7c3c1b3bc7828f93b0303d8bb5ececc1a2eec827
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-25 06:53:08 +0000

    Using the path module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
-var fs = require('fs')
+var fs = require('fs');
+var path = require('path');
 var server = http.createServer(function(req,res){
     console.log('Responding to a request');
     var url = req.url;
@@ -8,7 +9,8 @@ var server = http.createServer(function(req,res){
         fileName = url.substring(1);
     }
     console.log(fileName);
-    fs.readFile('app/index.html',function(err,data){
+    var filePath = path.resolve(__dirname,'app',fileName)
+    fs.readFile(filePath,function(err,data){
         res.end(data);
     })
 });
